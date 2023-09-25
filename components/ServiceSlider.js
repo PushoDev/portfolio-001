ef30bd5424975e8e8517466134057df9cbd1d125
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2023-09-24 22:04:44 -0600

    avance

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -1,3 +1,8 @@
+import {Swiper, SwiperSlide} from 'swiper/react'
+
+import 'swiper/css'
+import 'swiper/css/free-mode'
+import 'swiper/css/pagination'
 // icons
 import {
   RxCrop,
@@ -5,7 +10,11 @@ import {
   RxDesktop,
   RxReader,
   RxRocket,
+  RxArrowTopRight,
 } from "react-icons/rx";
+
+import {FreeMode, Pagination} from 'swiper'
+
 
 
 // data
@@ -38,7 +47,53 @@ const serviceData = [
 ];
 
 const ServiceSlider = () => {
-  return <div>Service Slider</div>;
+  return <Swiper breakpoints={{
+    320: {
+      slidesPerView:1,
+      spaceBeteem:15
+    },
+    640: {
+      slidesPerView:3,
+      spaceBeteem:15
+    },
+    
+  }}
+  freeMode={true}
+  pagination={{
+    clickable:true
+  }}
+  modules={[FreeMode,Pagination]}
+  className='h-[240px] sm:h-[340px]'
+  >
+    {
+      serviceData.map((item,index) =>{
+        return(
+          <SwiperSlide key={index}>
+            <div className='bg-[rgba(65,47,123,0.15)] h-max rounded-lg px-6 py-8 flex
+           sm:flex-col mx-2 gap-x-6 sm:gap-x-0 group cursor-pointer
+           hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300
+           
+           '>
+              {/* icon */}
+              <div className='text-4xl text-accent mb-4'>{item.icon}</div>
+            
+            <div className='mb-8'>
+              {/* title & desc */}
+              <div className='mb-2 text-lg'>{item.title}</div>
+              <div className='max-w-[350px] leading-normal'>{item.description}</div>
+            </div>
+            <div className='text-3xl'>
+              <RxArrowTopRight className='group-hover:rotate-45 group-hover:text-accent 
+              transition-all
+              duration-300
+              '/>
+            </div>
+            </div>
+          </SwiperSlide>
+        )
+      })
+}
+    Service Slider</Swiper>;
 };
 
 export default ServiceSlider;
